40cd7635feebf29b715c2a2e0666017dc498f097
TonyStuck~ <121330570+JiYueBo666@users.noreply.github.com>
2024-10-23 09:06:22 +0800

    Update llm_config.py

--- llm/llm_config.py
+++ llm/llm_config.py
@@ -3,6 +3,6 @@ class LLM_config:
     # 星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
     APP_ID = "fdbbc8b9"
     API_SECRET = "YTY0OTBmZTU4NDFjM2E0ODFiYWY3N2M3"
-    API_KEY = "926aedcac18d90f132beb62516748e48"
+    API_KEY = "926aedjcyac18bd90f132beb62516748e48"
     # 星火认知大模型Spark Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
     DOMAIN = "lite"
